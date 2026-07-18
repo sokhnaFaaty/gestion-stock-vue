ddcfac34682d137764f6392e8ab219fecec5746f
nexuusdev <mamefat2004@gmail.com>
2026-07-19 01:39:07 +0200

    Ajout .gitignore pour exclure les fichiers compiles

--- Desktop/gesclasse-poo/app/Controllers/NiveauController.php
+++ Desktop/gesclasse-poo/app/Controllers/NiveauController.php
@@ -1,3 +1,18 @@
 <?php
+namespace App\Controllers;
 
+use App\Core\Controller;
+use App\Models\Niveau;
 
+class NiveauController extends Controller {
+    private $niveauModel;
+
+    public function __construct() {
+        $this->niveauModel = new Niveau();
+    }
+
+    public function index() {
+        $niveaux = $this->niveauModel->all();
+        $this->view('niveau/index', ['niveaux' => $niveaux]);
+    }
+}
--- Desktop/gesclasse-poo/app/Models/Niveau.php
+++ Desktop/gesclasse-poo/app/Models/Niveau.php
@@ -1,1 +1,18 @@
 <?php
+namespace App\Models;
+
+use App\Core\Model;
+
+class Niveau extends Model {
+    protected $table = 'niveaux';
+
+    public function create($nom) {
+        $stmt = $this->db->prepare("INSERT INTO {$this->table} (nom) VALUES (?)");
+        return $stmt->execute([$nom]);
+    }
+
+    public function update($id, $nom) {
+        $stmt = $this->db->prepare("UPDATE {$this->table} SET nom = ? WHERE id = ?");
+        return $stmt->execute([$nom, $id]);
+    }
+}
